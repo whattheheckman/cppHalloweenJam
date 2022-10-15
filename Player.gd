a41fca60b4672ce01943925495587af2c91ef348
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2022-10-15 15:55:29 -0700

    player camera + exports + spotlight tweaks

--- Player.gd
+++ Player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var SPEED := 5.0
+
+@onready var _spring_arm: SpringArm3D = $CameraRig
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 
 
 func _physics_process(delta):
@@ -13,9 +15,6 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -27,5 +26,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	_spring_arm.translate(velocity)
 
 	move_and_slide()
